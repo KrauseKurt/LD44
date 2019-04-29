b1a66a293299ca23c747c421b811ed5651ce00d6
diego@playmove.com.br <diego@playmove.com.br>
2019-04-29 13:17:14 -0300

    Store Manager, Game Controller, gitignore
    Game Controller: all values the game stay here.
    Store Manager: here have a functions that store.

---
+++ Assets/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameController
+{
+    #region VARIABLES
+    /// <summary>
+    /// Counts in-game
+    /// </summary>
+    public static int Hearts, Coins, WaveEnemies, PowerDamage;
+
+    /// <summary>
+    /// 4 life equals 1 heart
+    /// </summary>
+    public static float Life;
+
+    /// <summary>
+    /// Wave that player stay playing
+    /// </summary>
+    public static int CurrentWave;
+
+    public static Player Player;
+
+
+
+    #endregion
+
+    #region METHODS
+
+
+    #endregion
+}
---
+++ Assets/StoreManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreManager : MonoBehaviour
+{
+    [Header("Prices")]
+    /// <summary>
+    /// Table price hearts
+    /// </summary>
+    public int[] PriceHearts = new int[10];
+    /// <summary>
+    /// Table price hearts
+    /// </summary>
+    public int[] PriceUpgrade = new int[10];
+
+    [Header("HUD")]
+    public Button ButtonBuyHearts;
+    public Button ButtonBuyUpgrade;
+
+    void Awake()
+    {
+        // Listens buttons
+        ButtonBuyHearts.onClick.AddListener(BuyHeart);
+        ButtonBuyUpgrade.onClick.AddListener(BuyUpgrade);
+    }
+
+    /// <summary>
+    /// Buy  one heart
+    /// </summary>
+    public void BuyHeart()
+    {
+        if (Pay(PriceHearts[GameController.Hearts]))
+        {
+            GameController.Life += 4;
+            GameController.Hearts += 1;
+            //GameController.Player.AddLife();
+        }
+    }
+
+    /// <summary>
+    /// Buy one upgrade
+    /// </summary>
+    public void BuyUpgrade()
+    {
+        if (Pay(PriceUpgrade[GameController.PowerDamage]))
+        {
+            GameController.PowerDamage += 4;
+            //GameController.Player.PowerDamage();
+        }
+    }
+
+    /// <summary>
+    /// Payed to buy anything
+    /// </summary>
+    /// <returns>If can to buy</returns>
+    private static bool Pay(int cust)
+    {
+        if (GameController.Coins > cust)
+        {
+            GameController.Coins -= cust;
+            return true;
+        }
+        return false;
+    }
+}
